46cf24f2f494e677cdd2d05e264e535e89127268
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-05-11 12:18:16 -0500

    fixed the configuration

---
+++ src/main/groovy/com/awesomecompany/gorm/InitGrailsApplicationFactoryBean.groovy
@@ -0,0 +1,18 @@
+package com.awesomecompany.gorm
+
+import org.codehaus.groovy.grails.commons.GrailsApplicationFactoryBean
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: domix
+ * Date: 11/05/12
+ * Time: 10:13
+ * To change this template use File | Settings | File Templates.
+ */
+class InitGrailsApplicationFactoryBean extends GrailsApplicationFactoryBean {
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    super.afterPropertiesSet()
+    super.getObject().initialise()
+  }
+}
