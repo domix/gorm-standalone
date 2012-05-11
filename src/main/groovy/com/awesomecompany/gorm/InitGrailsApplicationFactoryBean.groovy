a03e857879b335ec0a115d0abfc1a08c0d28754e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-05-11 12:45:02 -0500

    only format

--- src/main/groovy/com/awesomecompany/gorm/InitGrailsApplicationFactoryBean.groovy
+++ src/main/groovy/com/awesomecompany/gorm/InitGrailsApplicationFactoryBean.groovy
@@ -10,10 +10,11 @@ import org.codehaus.groovy.grails.commons.GrailsApplicationFactoryBean
  * To change this template use File | Settings | File Templates.
  */
 class InitGrailsApplicationFactoryBean extends GrailsApplicationFactoryBean {
-  @Override
+
   /**
    * You need to do this, because is necessary to explicitely call the initialise on GrailsApplication
    */
+  @Override
   public void afterPropertiesSet() throws Exception {
     super.afterPropertiesSet()
     super.getObject().initialise()
